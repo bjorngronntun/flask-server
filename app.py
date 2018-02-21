650518cb7a502f01368a19247868bb5e25191b65
bjorngronntun <bjorngronntun@gmail.com>
2018-02-21 19:42:43 +0100

    Trying new endpoints

--- app.py
+++ app.py
@@ -5,13 +5,25 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-with open('data/graph.json') as f:
-    graph = json.load(f)
+
 
 # For now, only one graph:
 @app.route('/')
 def draw_graph():
+    return 'Hello'
+
+@app.route('/graph1')
+def draw_graph1():
+    with open('data/graph1.json') as f:
+        graph = json.load(f)
     return jsonify(graph)
+
+@app.route('/graph2')
+def draw_graph2():
+    with open('data/graph2.json') as f:
+        graph = json.load(f)
+    return jsonify(graph)
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
